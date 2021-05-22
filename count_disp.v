005b33cd3a849c2bea6981d47ceab777b856cfd4
ssadeg10 <wildhunter360@gmail.com>
2021-05-21 19:40:32 -0700

    removed redundant files, added LEDs

--- count_disp.v
+++ count_disp.v
@@ -8,34 +8,40 @@
 
 module count_disp
 (
-input s0,s1,s2,s3,	// switches
-output reg [6:0] d	// 7-seg
+input wire [3:0] s,	// switches
+output reg [6:0] d,	// 7-seg
+output reg [9:0] l	// LEDs
 );
 	// Concat switches to 4-bit wire f
-	wire [3:0] f = {s3,s2,s1,s0};
-
-	// Bits for each 7-seg pin to display correct value
-	localparam ZERO		= 7'b1000000,
-		   ONE		= 7'b1111001,
-		   TWO		= 7'b0100100,
-		   THREE	= 7'b0110000,
-		   FOUR		= 7'b0011001,
-		   FIVE		= 7'b0010010,
-		   SIX		= 7'b0000010,
-		   SEVEN	= 7'b1111000,
-		   EIGHT	= 7'b0000000,
-		   NINE		= 7'b0011000,
-		   A		= 7'b0001000,
-		   B		= 7'b0000011,
-		   C		= 7'b1000110,
-		   D		= 7'b0100001,
-		   E		= 7'b0000100,
-		   F		= 7'b0001110,
-		   OFF		= 7'b1111111;
+	//wire [3:0] f = {s3,s2,s1,s0};
 	
-	// Output on switches --> set display
-	always @(f) begin
-		case(f)
+	// LEDs initially logic 0
+	initial begin
+		l = 10'b0;
+	end
+	
+	// Bits for each 7-seg pin to display correct value
+	localparam 	ZERO	= 7'b1000000,
+					ONE	= 7'b1111001,
+					TWO	= 7'b0100100,
+					THREE	= 7'b0110000,
+					FOUR	= 7'b0011001,
+					FIVE	= 7'b0010010,
+					SIX	= 7'b0000010,
+					SEVEN	= 7'b1111000,
+					EIGHT	= 7'b0000000,
+					NINE	= 7'b0011000,
+					A		= 7'b0001000,
+					B		= 7'b0000011,
+					C		= 7'b1000110,
+					D		= 7'b0100001,
+					E		= 7'b0000100,
+					F		= 7'b0001110,
+					OFF	= 7'b1111111;
+	
+	// Output switches to display
+	always @(s) begin
+		case(s)
 			4'b0000:  d = ZERO;
 			4'b0001:  d = ONE;
 			4'b0010:  d = TWO;
@@ -53,5 +59,7 @@ output reg [6:0] d	// 7-seg
 			4'b1110:  d = E;
 			4'b1111:  d = F;
 		endcase
+		
+		l[4:0] = s; // Output switches to LEDs
 	end
 endmodule
